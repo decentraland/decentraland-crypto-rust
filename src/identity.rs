5948ec3a6c67b9fb69022538d886a4a576c236db
frami <i@2fd.me>
2023-05-07 03:32:56 -0300

    feat: implement identity

--- src/identity.rs
+++ src/identity.rs
@@ -1,10 +1,66 @@
 use serde::{Serialize, Deserialize};
-use crate::account::{Account, Expiration};
-use crate::chain::AuthChain;
+use crate::AuthLink;
+use crate::account::{Account, Expiration, Signer, EphemeralPayload};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Identity {
     ephemeral_identity: Account,
-    auth_chain: AuthChain,
     expiration: Expiration,
+    auth_chain: Vec<AuthLink>,
+}
+
+impl Signer for Identity {
+    fn address(&self) -> crate::Address {
+        self.ephemeral_identity.address()
+    }
+
+    fn sign<M: AsRef<[u8]>>(&self, message: M) -> crate::account::PersonalSignature {
+        self.ephemeral_identity.sign(message)
+    }
+}
+
+impl Identity {
+    pub fn from_json<J: AsRef<str>>(json: J) -> Result<Self, serde_json::Error> {
+        serde_json::from_str::<Identity>(json.as_ref())
+    }
+
+    pub fn from_signer<S: Signer, E: Into<Expiration>>(signer: S, exp: E) -> Self {
+        let ephemeral_identity = Account::random();
+        let expiration = exp.into();
+        let address = signer.address();
+
+        let payload = EphemeralPayload::new(address, expiration);
+        let signature = signer.sign(payload.to_string());
+
+        Self {
+            ephemeral_identity,
+            expiration,
+            auth_chain: vec![
+                AuthLink::signer(address),
+                AuthLink::EcdsaPersonalEphemeral{ payload, signature }
+            ],
+        }
+    }
+
+    pub fn from_identity<E: Into<Expiration>>(identity: &Identity, exp: E) -> Self {
+        let ephemeral_identity = Account::random();
+        let address = identity.address();
+        let mut expiration = exp.into();
+        if identity.expiration < expiration {
+            expiration = identity.expiration;
+        }
+
+        let payload = EphemeralPayload::new(address, expiration);
+        let signature = identity.sign(payload.to_string());
+        let mut auth_chain: Vec<AuthLink> = identity.auth_chain.clone();
+        auth_chain.push(AuthLink::EcdsaPersonalEphemeral{ payload, signature });
+
+        Self {
+            ephemeral_identity,
+            expiration,
+            auth_chain
+        }
+
+    }
+
 }
